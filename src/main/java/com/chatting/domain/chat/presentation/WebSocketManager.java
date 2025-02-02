8250f3d68bdd33441b809721e6b8afbbe1c9b499
mini0192 <parkgw9071@gmail.com>
2025-02-02 21:41:21 +0900

    Refacor: 코드 정리

--- src/main/java/com/chatting/domain/chat/presentation/WebSocketManager.java
+++ src/main/java/com/chatting/domain/chat/presentation/WebSocketManager.java
@@ -13,7 +13,6 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @Controller
 @RequiredArgsConstructor
